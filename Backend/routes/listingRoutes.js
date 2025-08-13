f217b51c7f3187ff2fc914cd4145ca61c20928b0
Firagos <jemalfiragos@gmail.com>
2025-08-13 08:02:31 +0300

    creating the listing routes

---
+++ Backend/routes/listingRoutes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { protect } from "../middlewares/authMiddleware.js";
+import { upload } from "../middlewares/upload.js";
+import {
+  listListings,
+  getListingById,
+  createListing,
+  updateListing,
+  deleteListing,
+} from "../controllers/listingController.js";
+
+const router = Router();
+
+router.get("/", listListings);
+router.get("/:id", getListingById);
+router.post("/", protect, upload.array("images", 10), createListing);
+router.patch("/:id", protect, upload.array("images", 10), updateListing);
+router.delete("/:id", protect, deleteListing);
+
+export default router;
