3cb199d5114291da4ce05cebf3ad3abbd5658924
Firagos <jemalfiragos@gmail.com>
2025-08-19 11:40:27 +0300

    adding the navbar to it

--- Client/src/pages/HomePage.jsx
+++ Client/src/pages/HomePage.jsx
@@ -5,12 +5,14 @@ import { FeaturesBanner } from "../components/FeaturesBanner";
 import { BlogSection } from "../components/BlogSection";
 import { Footer } from "../components/Footer";
 import PopularProducts from "../components/PopulraProducts";
+import Navbar from "../components/Navbar";
 
 export default function HomePage() {
   return (
     <div className="bg-[#fff]">
       {/* Page Wrapper with width 85rem */}
       <div className="max-w-[85rem] mx-auto px-4">
+        <Navbar />
         <HeroSection />
         <FeaturesBanner />
         <PopularProducts />
