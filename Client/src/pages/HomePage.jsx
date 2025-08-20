1f9aa046b4f2672ed0e2102f826535c4045c319f
Firagos <jemalfiragos@gmail.com>
2025-08-20 08:44:15 +0300

    testing the cart

--- Client/src/pages/HomePage.jsx
+++ Client/src/pages/HomePage.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { HeroSection } from "../components/HeroSection";
-import ProductSection from "../components/ProductSection";
+import MostSoldProductSection from "../components/MostSoldProductSection";
 import { FeaturesBanner } from "../components/FeaturesBanner";
 import { BlogSection } from "../components/BlogSection";
 import { Footer } from "../components/Footer";
-import PopularProducts from "../components/PopulraProducts";
+import PopularProducts from "../components/PopularProducts";
 import Navbar from "../components/Navbar";
 
-export default function HomePage() {
+export default function HomePage({ openCart, cartItems, total }) {
   return (
     <div className="bg-[#fff]">
       {/* Page Wrapper with width 85rem */}
       <div className="max-w-[85rem] mx-auto px-4">
-        <Navbar />
+        {/* ✅ Pass openCart & cartItems into Navbar */}
+        <Navbar openCart={openCart} cartItems={cartItems} total={total} />
+
         <HeroSection />
         <FeaturesBanner />
         <PopularProducts />
-        <ProductSection />
+        <MostSoldProductSection />
         <BlogSection />
       </div>
 
