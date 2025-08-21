e984f68b421e101a3d4274413782bce4b70031fa
Firagos <jemalfiragos@gmail.com>
2025-08-21 11:40:14 +0300

    making the close button to look more good

--- Client/src/components/AuthModal.jsx
+++ Client/src/components/AuthModal.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import { FaTimes } from "react-icons/fa";
 
 export default function AuthModal({ isOpen, onClose }) {
   const [isLogin, setIsLogin] = useState(true);
@@ -20,7 +21,7 @@ export default function AuthModal({ isOpen, onClose }) {
 
           {/* Modal Drawer */}
           <motion.div
-            className="fixed top-0 right-0 h-full w-full max-w-md bg-white shadow-2xl z-50 flex flex-col p-8"
+            className="fixed top-0 right-0 h-full w-full max-w-md bg-white shadow-2xl z-50 flex flex-col p-8 justify-center"
             initial={{ x: "100%" }}
             animate={{ x: 0 }}
             exit={{ x: "100%" }}
@@ -29,18 +30,18 @@ export default function AuthModal({ isOpen, onClose }) {
             {/* Close button */}
             <button
               onClick={onClose}
-              className="absolute top-4 right-4 text-gray-500 hover:text-gray-800 text-xl font-bold"
+              className="absolute top-4 right-4 p-2 hover:text-gray-800 hover:bg-gray-100 hover:cursor-pointer rounded-full"
             >
-              ✕
+              <FaTimes className="w-6 h-6" />
             </button>
 
             {/* Header */}
-            <h2 className="text-2xl font-bold text-center text-blue-600 mb-6">
+            <h2 className="text-2xl font-bold text-center text-[#F9A03F] mb-6">
               {isLogin ? "Welcome Back 👋" : "Create an Account 🚀"}
             </h2>
 
             {/* Form */}
-            <form className="space-y-4">
+            <form className="space-y-4 ">
               {!isLogin && (
                 <input
                   type="text"
@@ -66,7 +67,7 @@ export default function AuthModal({ isOpen, onClose }) {
                 />
               )}
 
-              <button className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition">
+              <button className="w-full bg-[#F9A03F] text-white py-2 rounded-lg hover:bg-blue-700 transition">
                 {isLogin ? "Login" : "Sign Up"}
               </button>
             </form>
@@ -79,7 +80,7 @@ export default function AuthModal({ isOpen, onClose }) {
                   <button
                     type="button"
                     onClick={() => setIsLogin(false)}
-                    className="text-blue-600 font-semibold hover:underline"
+                    className="text-[#f9A03f] font-semibold hover:underline"
                   >
                     Sign Up
                   </button>
@@ -90,7 +91,7 @@ export default function AuthModal({ isOpen, onClose }) {
                   <button
                     type="button"
                     onClick={() => setIsLogin(true)}
-                    className="text-blue-600 font-semibold hover:underline"
+                    className="text-[#f9A03f]font-semibold hover:underline"
                   >
                     Login
                   </button>
