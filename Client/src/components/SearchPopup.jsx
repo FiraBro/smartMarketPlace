b0aa863b52480f4564851c3479123a283167719a
Firagos <jemalfiragos@gmail.com>
2025-09-01 19:50:23 +0300

    simple modification

--- Client/src/components/SearchPopup.jsx
+++ Client/src/components/SearchPopup.jsx
@@ -12,7 +12,27 @@ export default function SearchPopup({
   return (
     <div className="absolute top-14 left-0 w-full bg-white border border-gray-200 rounded-xl shadow-lg z-50 max-h-96 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100 p-3">
       {results.length > 0 ? (
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        // <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-70">
+        //   {results.map((item) => (
+        //     <ProductCard
+        //       key={item._id}
+        //       product={{
+        //         id: item._id,
+        //         name: item.title,
+        //         price: item.price,
+        //         image: item.images?.[0]
+        //           ? `${
+        //               import.meta.env.VITE_API_URL || "http://localhost:5000"
+        //             }${item.images[0]}`
+        //           : "https://via.placeholder.com/200",
+        //         rating: item.rating || 0,
+        //         reviews: item.reviews || 0,
+        //       }}
+        //       onAddToCart={onAddToCart}
+        //     />
+        //   ))}
+        // </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 max-w-5xl mx-auto">
           {results.map((item) => (
             <ProductCard
               key={item._id}
