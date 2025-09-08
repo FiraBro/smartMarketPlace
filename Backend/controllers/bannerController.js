2f1ec77c6a037fcd019297cef7b9d4b033e8cd2b
Firagos <jemalfiragos@gmail.com>
2025-09-08 16:36:23 +0300

    creating the banner controller

---
+++ Backend/controllers/bannerController.js
@@ -0,0 +1,43 @@
+// controllers/bannerController.js
+import Banner from "../models/Banner.js";
+import catchAsync from "../utils/catchAsync.js";
+import AppError from "../utils/AppError.js";
+import fs from "fs";
+import path from "path";
+
+export const uploadBanner = catchAsync(async (req, res, next) => {
+  if (!req.file) {
+    return next(new AppError("Please upload an image", 400));
+  }
+
+  // Get all banners (sorted by oldest first)
+  const banners = await Banner.find().sort({ createdAt: 1 });
+
+  // If already 5, remove the oldest
+  if (banners.length >= 5) {
+    const oldest = banners[0];
+
+    // Delete file from uploads directory
+    if (oldest.image) {
+      const oldPath = path.join(process.cwd(), oldest.image);
+      fs.unlink(oldPath, (err) => {
+        if (err) console.error("Error deleting old banner:", err);
+      });
+    }
+
+    // Remove from DB
+    await Banner.findByIdAndDelete(oldest._id);
+  }
+
+  // Save new banner to DB
+  const newBanner = await Banner.create({
+    image: `uploads/${req.file.filename}`, // relative path
+  });
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      banner: newBanner,
+    },
+  });
+});
