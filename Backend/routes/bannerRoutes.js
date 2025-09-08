a2111a61eb2f69767edae3cddeb0f7f88ca09a06
Firagos <jemalfiragos@gmail.com>
2025-09-08 16:37:29 +0300

    creating the banner routes

--- Backend/routes/bannerRoutes.js
+++ Backend/routes/bannerRoutes.js
@@ -1,25 +1,9 @@
-import { Router } from "express";
-import {
-  createBanner,
-  getActiveBanners,
-  updateBanner,
-  deleteBanner,
-} from "../controllers/banner.controller.js";
-import { cacheTTL } from "../middleware/cacheTTL.js";
-import { isAdmin } from "../middleware/isAdmin.js";
-// Use your existing multer middleware (upload.array('images', 10))
-import upload from "../middleware/upload.js"; // assumed existing
+// routes/bannerRoutes.js
+import express from "express";
+import { uploadBanner } from "../controllers/bannerController.js";
+import { upload } from "../middlewares/upload.js";
+const bannerRoutes = express.Router();
 
-const router = Router();
+bannerRoutes.post("/", upload.single("image"), uploadBanner);
 
-router.get(
-  "/",
-  cacheTTL(() => "home:banners", 5 * 60_000),
-  getActiveBanners
-);
-
-router.post("/", isAdmin, upload.array("images", 10), createBanner);
-router.patch("/:id", isAdmin, upload.array("images", 10), updateBanner);
-router.delete("/:id", isAdmin, deleteBanner);
-
-export default router;
+export default bannerRoutes;
