7dcc9455bf47ffb2efc94f537dfcff0ebf4779eb
Firagos <jemalfiragos@gmail.com>
2025-09-08 16:48:42 +0300

    adding the fetching route to it

--- Backend/routes/bannerRoutes.js
+++ Backend/routes/bannerRoutes.js
@@ -1,9 +1,10 @@
 // routes/bannerRoutes.js
 import express from "express";
-import { uploadBanner } from "../controllers/bannerController.js";
+import { uploadBanner, getBanners } from "../controllers/bannerController.js";
 import { upload } from "../middlewares/upload.js";
 const bannerRoutes = express.Router();
 
 bannerRoutes.post("/", upload.single("image"), uploadBanner);
+bannerRoutes.get("/", getBanners);
 
 export default bannerRoutes;
