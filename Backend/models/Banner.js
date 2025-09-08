0017668008d6d5570c6507305fe290fb69b163f9
Firagos <jemalfiragos@gmail.com>
2025-09-08 16:37:00 +0300

    creating the banner model

--- Backend/models/Banner.js
+++ Backend/models/Banner.js
@@ -1,11 +1,15 @@
-// models/banner.model.js
+// models/bannerModel.js
 import mongoose from "mongoose";
 
 const bannerSchema = new mongoose.Schema(
   {
-    images: [String],
+    image: {
+      type: String,
+      required: [true, "A banner must have an image"],
+    },
   },
   { timestamps: true }
 );
 
-export default mongoose.model("Banner", bannerSchema);
+const Banner = mongoose.model("Banner", bannerSchema);
+export default Banner;
