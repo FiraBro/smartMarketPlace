16505bd8f1c32977160d4a0435bc3089fa62d4d4
Firagos <jemalfiragos@gmail.com>
2025-09-16 17:41:48 +0300

    adding get all listing route to it

--- Backend/routes/listingRoutes.js
+++ Backend/routes/listingRoutes.js
@@ -7,11 +7,13 @@ import {
   createListing,
   updateListing,
   deleteListing,
+  getAllListings,
 } from "../controllers/listingController.js";
 
 const router = Router();
 
 router.get("/", listListings);
+router.get("/all", getAllListings); // paginated, no filters
 router.get("/:id", getListingById);
 router.post("/", protect, upload.array("images", 10), createListing);
 router.patch("/:id", protect, upload.array("images", 10), updateListing);
