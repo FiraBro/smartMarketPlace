7c729821787d856a856d7b4b6f89aa7ce347dec8
Firagos <jemalfiragos@gmail.com>
2025-09-26 20:04:33 +0300

    creating address model

---
+++ Backend/models/Address.js
@@ -0,0 +1,15 @@
+// models/Address.js
+import mongoose from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  name: { type: String, required: true }, // e.g., John Doe
+  street: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String },
+  zip: { type: String, required: true },
+  country: { type: String, required: true },
+  phone: { type: String, required: true },
+}, { timestamps: true });
+
+export default mongoose.model("Address", addressSchema);
