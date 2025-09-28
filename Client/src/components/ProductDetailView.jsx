ccf2a36923447eb5de1446005886845b18b6cf97
Firagos <jemalfiragos@gmail.com>
2025-09-28 08:30:56 +0300

    fixing the error image is not showing

--- Client/src/components/ProductDetailView.jsx
+++ Client/src/components/ProductDetailView.jsx
@@ -10,7 +10,7 @@ export default function ProductDetailView({
   isLoading = false,
   error = null,
 }) {
-  const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+  const BASE_URL = import.meta.env.VITE_STATIC_URL || "http://localhost:5000";
   const [mainImage, setMainImage] = useState("");
   const [imageLoading, setImageLoading] = useState(true);
 
