7bce144af628fa25908e904becb256e1cb01f689
Firagos <jemalfiragos@gmail.com>
2025-10-02 11:01:20 +0300

    changing the grid

--- Client/src/components/SearchPopup.jsx
+++ Client/src/components/SearchPopup.jsx
@@ -13,7 +13,7 @@ export default function SearchPopup({
       <h2 className="text-xl font-semibold mb-6">Search Results</h2>
 
       {results.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
           {results.map((item) => (
             <div
               key={item._id}
