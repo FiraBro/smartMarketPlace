815e048761d2162aac1d111f9d16c5d5a182f051
Firagos <jemalfiragos@gmail.com>
2025-10-03 09:59:21 +0300

    fixing the import

--- Client/src/pages/HomePage.jsx
+++ Client/src/pages/HomePage.jsx
@@ -3,9 +3,7 @@ import { HeroSection } from "../components/HeroSection";
 import MostSoldProductSection from "../components/MostSoldProductSection";
 import { CategorySection } from "../components/CategorySection";
 import { SubscriptionSection } from "../components/SubscriptionSection";
-import { Footer } from "../components/Footer";
 import PopularProducts from "../components/PopularProducts";
-import Navbar from "../components/Navbar";
 
 export default function HomePage() {
   return (
