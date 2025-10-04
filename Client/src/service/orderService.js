9dcaf94fcf8bfbae8993d4ec1a1f18a0ece68802
Firagos <jemalfiragos@gmail.com>
2025-10-04 11:35:34 +0300

    fixing the create order

--- Client/src/service/orderService.js
+++ Client/src/service/orderService.js
@@ -24,6 +24,7 @@ export const createOrder = async ({
   address,
   paymentMethod,
   totalPrice,
+  deliveryMethod,
 }) => {
   try {
     const response = await orderAPI.post("/", {
@@ -31,6 +32,7 @@ export const createOrder = async ({
       address, // string (_id)
       paymentMethod, // "COD" or "TeleBirr"
       totalPrice,
+      deliveryMethod,
     });
     return response.data;
   } catch (error) {
@@ -43,5 +45,25 @@ export const createOrder = async ({
 // Get user orders
 export const getOrders = async () => {
   const { data } = await orderAPI.get("/myorders");
+  return data;
+};
+export const getOrderById = async (orderId) => {
+  const { data } = await orderAPI.get(`/${orderId}`);
+  return data;
+};
+export const payWithCOD = async (orderId) => {
+  const { data } = await orderAPI.post(`/${orderId}/pay/cod`);
+  return data;
+};
+export const payWithTeleBirr = async (orderId, phone) => {
+  const { data } = await orderAPI.post(`/${orderId}/pay/telebirr`, { phone });
+  return data;
+};
+export const confirmOrder = async (orderId) => {
+  const { data } = await orderAPI.post(`/${orderId}/confirm`);
+  return data;
+};
+export const cancelOrder = async (orderId) => {
+  const { data } = await orderAPI.post(`/${orderId}/cancel`);
   return data;
 };
