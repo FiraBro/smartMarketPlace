1c3a785f744ddae39cb20d6d862c458aab88b242
Firagos <jemalfiragos@gmail.com>
2025-10-09 22:22:33 +0300

    creating seller layout

---
+++ Client/src/util/SellerLayout.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import SellerSidebar from "../components/seller/SellerSidebar";
+import SellerNavbar from "../components/seller/SellerNavbar";
+
+export default function SellerLayout({ children }) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => setSidebarOpen((prev) => !prev);
+
+  return (
+    <div className="flex h-screen bg-gray-50 overflow-hidden">
+      {/* Sidebar */}
+      <SellerSidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
+
+      {/* Main content */}
+      <div className="flex-1 flex flex-col overflow-y-auto lg:ml-auto">
+        <SellerNavbar toggleSidebar={toggleSidebar} />
+        <div className="p-4 sm:p-6 md:p-8 flex-1">{children}</div>
+      </div>
+    </div>
+  );
+}
