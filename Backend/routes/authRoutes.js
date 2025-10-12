a17090d788a28f8f0bf322bc8b5b18ce05fedf5f
Firagos <jemalfiragos@gmail.com>
2025-10-12 11:11:56 +0300

    modifiying it

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,20 +1,22 @@
 import express from "express";
-import {
-  registerUser,
-  loginUser,
-  getMe,
-  updateMe,
-} from "../controllers/authController.js";
 import { protect } from "../middlewares/authMiddleware.js";
+import * as authController from "../controllers/authController.js";
+import * as oauthController from "../controllers/oauthController.js";
 
 const router = express.Router();
 
-// Registration Route
-router.post("/register", registerUser);
+// Email/password routes
+router.post("/register", authController.registerUser);
+router.post("/login", authController.loginUser);
+router.get("/me", protect, authController.getMe);
+router.put("/me", protect, authController.updateMe);
 
-// Login Route
-router.post("/login", loginUser);
-router.get("/me", protect, getMe);
-router.put("/me", protect, updateMe);
+// GitHub OAuth
+router.get("/github", oauthController.githubLogin);
+router.get("/github/callback", oauthController.githubCallback);
+
+// Facebook OAuth
+router.get("/facebook", oauthController.facebookLogin);
+router.get("/facebook/callback", oauthController.facebookCallback);
 
 export default router;
