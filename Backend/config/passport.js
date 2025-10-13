c5aa3885ed14e916d0c4b06dd466d7c4e9ba54b6
Firagos <jemalfiragos@gmail.com>
2025-10-13 09:32:41 +0300

    refactoring it

--- Backend/config/passport.js
+++ Backend/config/passport.js
@@ -1,8 +1,6 @@
 import passport from "passport";
 import { Strategy as GitHubStrategy } from "passport-github2";
-import { Strategy as FacebookStrategy } from "passport-facebook";
 import { User } from "../models/User.js";
-import jwt from "jsonwebtoken";
 
 passport.serializeUser((user, done) => done(null, user.id));
 passport.deserializeUser(async (id, done) => {
@@ -10,7 +8,7 @@ passport.deserializeUser(async (id, done) => {
   done(null, user);
 });
 
-// 🔹 GitHub Strategy
+// GitHub Strategy
 passport.use(
   new GitHubStrategy(
     {
@@ -19,40 +17,24 @@ passport.use(
       callbackURL: process.env.GITHUB_CALLBACK_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
-      let user = await User.findOne({ email: profile.emails[0].value });
-      if (!user) {
-        user = await User.create({
-          name: profile.displayName || profile.username,
-          email: profile.emails[0].value,
-          password: "", // password not needed
-          role: "user",
-        });
-      }
-      done(null, user);
-    }
-  )
-);
+      try {
+        const email = profile.emails?.[0]?.value;
+        if (!email) return done(new Error("Email not provided by GitHub"));
 
-// 🔹 Facebook Strategy
-passport.use(
-  new FacebookStrategy(
-    {
-      clientID: process.env.FACEBOOK_APP_ID,
-      clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: process.env.FACEBOOK_CALLBACK_URL,
-      profileFields: ["id", "displayName", "emails"],
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      let user = await User.findOne({ email: profile.emails[0].value });
-      if (!user) {
-        user = await User.create({
-          name: profile.displayName,
-          email: profile.emails[0].value,
-          password: "",
-          role: "user",
-        });
+        let user = await User.findOne({ email });
+        if (!user) {
+          user = await User.create({
+            name: profile.displayName || profile.username,
+            email,
+            password: "",
+            role: "user",
+          });
+        }
+
+        done(null, user);
+      } catch (err) {
+        done(err, null);
       }
-      done(null, user);
     }
   )
 );
