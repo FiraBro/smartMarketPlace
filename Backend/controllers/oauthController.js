6a19f139e1f3c7a7f821218d608e6072b14fc6fd
Firagos <jemalfiragos@gmail.com>
2025-10-13 09:31:22 +0300

    adding generate token to it

--- Backend/controllers/oauthController.js
+++ Backend/controllers/oauthController.js
@@ -1,5 +1,5 @@
 import passport from "passport";
-import jwt from "jsonwebtoken";
+import { generateToken } from "../utils/generateToken.js";
 
 export const githubLogin = passport.authenticate("github", {
   scope: ["user:email"],
@@ -8,23 +8,7 @@ export const githubLogin = passport.authenticate("github", {
 export const githubCallback = [
   passport.authenticate("github", { failureRedirect: "/login" }),
   (req, res) => {
-    const token = jwt.sign({ id: req.user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1d",
-    });
-    res.redirect(`${process.env.FRONTEND_URL}/auth?token=${token}`);
-  },
-];
-
-export const facebookLogin = passport.authenticate("facebook", {
-  scope: ["email"],
-});
-
-export const facebookCallback = [
-  passport.authenticate("facebook", { failureRedirect: "/login" }),
-  (req, res) => {
-    const token = jwt.sign({ id: req.user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1d",
-    });
+    const token = generateToken(req.user._id);
     res.redirect(`${process.env.FRONTEND_URL}/auth?token=${token}`);
   },
 ];
