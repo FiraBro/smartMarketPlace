da5666a9b743befa562de019eff3d1ab2566b377
Firagos <jemalfiragos@gmail.com>
2025-10-17 23:23:23 +0300

    remiving thw username

--- Backend/models/Admin.js
+++ Backend/models/Admin.js
@@ -3,13 +3,6 @@ import bcrypt from "bcryptjs";
 
 const adminSchema = new mongoose.Schema(
   {
-    username: {
-      type: String,
-      required: [true, "Username is required"],
-      unique: true,
-      trim: true,
-      minlength: [3, "Username must be at least 3 characters long"],
-    },
     email: {
       type: String,
       required: [true, "Email is required"],
