ebcd5643565da8ee5d9f054e029c6e28b448d4c7
Firagos <jemalfiragos@gmail.com>
2025-10-17 23:13:31 +0300

    adding two more endpoint

--- AdminDashboard/src/services/authService.js
+++ AdminDashboard/src/services/authService.js
@@ -1,27 +1,40 @@
+// src/services/authService.js
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL_AUTH || "http://localhost:4000/api/auth";
+  import.meta.env.VITE_API_BASE_URL_AUTH || "http://localhost:5000/api/admin";
 
 // Always send cookies (sessions)
 axios.defaults.withCredentials = true;
 
 export const registerUser = async (data) => {
-  const res = await axios.post(`${API_BASE_URL}/register`, data);
+  const res = await axios.post(`${API_BASE_URL}/registerAdmin`, data);
   return res.data;
 };
 
 export const loginUser = async (data) => {
-  const res = await axios.post(`${API_BASE_URL}/login`, data);
+  const res = await axios.post(`${API_BASE_URL}/loginAdmin`, data);
+  console.log("Login successful: ", res.data);
   return res.data;
 };
 
 export const logoutUser = async () => {
-  const res = await axios.post(`${API_BASE_URL}/logout`);
+  const res = await axios.post(`${API_BASE_URL}/logoutAdmin`);
   return res.data;
 };
 
 export const getCurrentUser = async () => {
-  const res = await axios.get(`${API_BASE_URL}/me`);
-  return res.data.user;
+  const res = await axios.get(`${API_BASE_URL}/meAdmin`);
+  console.log("getCurrentUser response data:", res.data);
+  return res.data.data.admin; // important: return the admin object
+};
+export const getAllBuyer = async () => {
+  const res = await axios.get(`${API_BASE_URL}/buyers`);
+  console.log("getAllBuyer response data:", res.data);
+  return res.data;
+};
+
+export const getAllSellers = async () => {
+  const res = await axios.get(`${API_BASE_URL}/sellers`);
+  return res.data;
 };
