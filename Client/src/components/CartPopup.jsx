ab9f0f91f1099bdb88ae01533e270cd5fb972855
Firagos <jemalfiragos@gmail.com>
2025-10-18 20:33:02 +0300

    debuging it

--- Client/src/components/CartPopup.jsx
+++ Client/src/components/CartPopup.jsx
@@ -14,6 +14,7 @@ import {
 const CartPopup = ({ isOpen, onClose }) => {
   const { cart, increaseQuantity, decreaseQuantity, removeItem, clear } =
     useCart();
+  console.log(cart);
   const navigate = useNavigate();
 
   const [payment, setPayment] = useState("COD"); // COD, Telebirr, Chapa
