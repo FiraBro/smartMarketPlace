c52487b14829cfedfc1544a247115552437be55c
Firagos Jemal <jemalfiragos@gmail.com>
2025-10-29 22:32:16 +0300

    creating the buyer page

---
+++ Client/src/pages/buyer/BuyerNotificationsPage.jsx
@@ -0,0 +1,6 @@
+// src/pages/BuyerNotificationsPage.jsx
+import NotificationList from "../../components/NotificationList";
+
+export default function BuyerNotificationsPage() {
+  return <NotificationList userType="Buyer" />;
+}
