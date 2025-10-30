b1bf487db96e738e638a30fd4ce3b355d57ddf47
Firagos Jemal <jemalfiragos@gmail.com>
2025-10-30 20:43:51 +0300

    debuging it

--- Client/src/service/notificationService.js
+++ Client/src/service/notificationService.js
@@ -15,6 +15,7 @@ const API = axios.create({
 export const fetchNotifications = async (page = 1, limit = 10) => {
   try {
     const response = await API.get("/user", { params: { page, limit } });
+    console.log(response)
     return response.data;
   } catch (error) {
     console.error("Error fetching notifications:", error.response?.data || error.message);
