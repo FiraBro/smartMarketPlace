7f79e4217d4f7d55aa1535e6a38a25c1a7969e86
Firagos Jemal <jemalfiragos@gmail.com>
2025-11-02 23:29:26 +0300

    modifying it

--- AdminDashboard/src/services/notificationApi.js
+++ AdminDashboard/src/services/notificationApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
-  "http://localhost:5000/api/notifications";
+  "http://localhost:5000/api/admin";
 
 // Configure Axios to send cookies with every request
 axios.defaults.withCredentials = true;
@@ -11,7 +11,7 @@ axios.defaults.withCredentials = true;
 
 // Send a new notification (admin only)
 export const sendNotification = async (data) => {
-  const res = await axios.post(`${API_BASE_URL}/admin`, data);
+  const res = await axios.post(`${API_BASE_URL}/notifications`, data);
   return res.data;
 };
 
@@ -19,7 +19,7 @@ export const sendNotification = async (data) => {
 export const getNotificationHistory = async (filters = {}) => {
   // Build query string from filters
   const queryString = new URLSearchParams(filters).toString(); // channel=email&status=sent
-  const url = `${API_BASE_URL}/admin${queryString ? `?${queryString}` : ""}`;
+  const url = `${API_BASE_URL}/notifications/${queryString ? `?${queryString}` : ""}`;
 
   const res = await axios.get(url);
   return res.data.notifications;
@@ -27,22 +27,25 @@ export const getNotificationHistory = async (filters = {}) => {
 
 // Get single notification by ID (admin only)
 export const getNotificationById = async (id) => {
-  const res = await axios.get(`${API_BASE_URL}/admin/${id}`);
+  const res = await axios.get(`${API_BASE_URL}/${id}`);
   return res.data.notification;
 };
 
 // Delete notification by ID (admin only)
 export const deleteNotification = async (id) => {
-  const res = await axios.delete(`${API_BASE_URL}/admin/${id}`);
+  const res = await axios.delete(`${API_BASE_URL}/notifications/${id}`);
+  console.log(res)
+
   return res.data;
 };
-
 // ============================== USER SERVICES ============================== //
 
-// Get notifications for the logged-in user
-export const getUserNotifications = async () => {
-  const res = await axios.get(API_BASE_URL);
-  return res.data.notifications;
+// Fetch notifications for logged-in user with pagination
+export const fetchNotifications = async (page = 1, limit = 10) => {
+  const res = await axios.get(`${API_BASE_URL}/user`, {
+    params: { page, limit },
+  });
+  return res.data; // includes notifications + pagination
 };
 
 // Mark a specific notification as read
@@ -56,3 +59,5 @@ export const markAllAsRead = async () => {
   const res = await axios.patch(`${API_BASE_URL}/read-all`);
   return res.data;
 };
+
+
