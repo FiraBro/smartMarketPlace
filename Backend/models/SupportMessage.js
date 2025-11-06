8c0fc0ab69a548be18ee77b6fe9449673f81b397
Firagos Jemal <jemalfiragos@gmail.com>
2025-11-06 22:09:57 +0300

    modifying it

--- Backend/models/SupportMessage.js
+++ Backend/models/SupportMessage.js
@@ -1,23 +1,18 @@
-import Joi from "joi";
+import mongoose from "mongoose";
 
-export const supportMessageSchema = Joi.object({
-  name: Joi.string().min(3).max(100).required().messages({
-    "string.empty": "Full name is required",
-    "string.min": "Name must be at least 3 characters",
-  }),
+const supportMessageSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    subject: { type: String, required: true },
+    message: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["pending", "read", "replied"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
 
-  email: Joi.string().email().required().messages({
-    "string.empty": "Email is required",
-    "string.email": "Invalid email format",
-  }),
-
-  subject: Joi.string().min(3).max(150).required().messages({
-    "string.empty": "Subject is required",
-    "string.min": "Subject must be at least 3 characters",
-  }),
-
-  message: Joi.string().min(10).max(2000).required().messages({
-    "string.empty": "Message content is required",
-    "string.min": "Message must be at least 10 characters",
-  }),
-});
+export default mongoose.model("SupportMessage", supportMessageSchema);
