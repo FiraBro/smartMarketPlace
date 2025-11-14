9ccc53eeccab85a3f9880b55d9ab88c0bdaf3ac1
Firagos Jemal <jemalfiragos@gmail.com>
2025-11-14 23:29:52 +0300

    adding the image public id

--- Backend/models/Banner.js
+++ Backend/models/Banner.js
@@ -1,15 +1,13 @@
-// models/bannerModel.js
 import mongoose from "mongoose";
 
 const bannerSchema = new mongoose.Schema(
   {
-    image: {
-      type: String,
-      required: [true, "A banner must have an image"],
-    },
+    image: { type: String, required: true }, // Cloudinary URL
+    imagePublicId: { type: String, required: true }, // Cloudinary public_id for deletion
   },
   { timestamps: true }
 );
 
 const Banner = mongoose.model("Banner", bannerSchema);
+
 export default Banner;
