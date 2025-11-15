6d243f5214543173705bafedfcb973e14dd8870c
Firagos Jemal <jemalfiragos@gmail.com>
2025-11-15 22:13:36 +0300

    add seed forst util

--- Backend/server.js
+++ Backend/server.js
@@ -1,70 +1,3 @@
-// import "dotenv/config";
-// import http from "http";
-// import { Server } from "socket.io";
-// import { connectDB } from "./config/db.js";
-// import app from "./app.js";
-// import { protectSocket } from "./middlewares/socketAuthMiddleware.js";
-
-// // ----------------------------
-// // Connect to MongoDB
-// // ----------------------------
-// connectDB();
-
-// // ----------------------------
-// // Create HTTP server
-// // ----------------------------
-// const server = http.createServer(app);
-
-// // ----------------------------
-// // Socket.io setup
-// // ----------------------------
-// const io = new Server(server, {
-//   cors: {
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true,
-//   },
-// });
-
-// // Socket authentication
-// io.use(protectSocket);
-
-// io.on("connection", (socket) => {
-//   console.log(`User connected: ${socket.user.name}`);
-
-//   // ----------------------------
-//   // Chat rooms
-//   // ----------------------------
-//   socket.on("joinChat", (chatId) => {
-//     socket.join(`chat:${chatId}`);
-//   });
-
-//   socket.on("sendMessage", (message) => {
-//     io.to(`chat:${message.chatId}`).emit("messageReceived", message);
-//   });
-
-//   // ----------------------------
-//   // Notifications
-//   // ----------------------------
-//   // Join a personal notification room automatically
-//   socket.join(`notifications:${socket.user._id}`);
-//   console.log(`User ${socket.user._id} joined notifications room`);
-
-//   socket.on("disconnect", () => {
-//     console.log(`User disconnected: ${socket.user.name}`);
-//   });
-// });
-
-// // Make io accessible in controllers
-// app.set("io", io);
-
-// // ----------------------------
-// // Start server
-// // ----------------------------
-// const PORT = process.env.PORT || 5000;
-// server.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
 import "dotenv/config";
 import http from "http";
 import { Server } from "socket.io";
@@ -72,11 +5,16 @@ import { connectDB } from "./config/db.js";
 import app from "./app.js";
 import { protectSocket } from "./middlewares/socketAuthMiddleware.js";
 import { sessionConfig } from "./config/session.js"; // your session import
-
+import { seedFirstAdmin } from "./utils/seedFirstAdmin.js";
 // ----------------------------
 // Connect to MongoDB
 // ----------------------------
-connectDB();
+await connectDB(); // make sure connectDB returns a promise
+
+// ----------------------------
+// Seed first admin if none exists
+// ----------------------------
+await seedFirstAdmin(); // ✅ creates default admin automatically
 
 // ----------------------------
 // Create HTTP server
@@ -84,11 +22,12 @@ connectDB();
 const server = http.createServer(app);
 
 // ----------------------------
-// Socket.io setup ----------------------------
+// Socket.io setup
+// ----------------------------
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL, // e.g. http://localhost:3000
-    credentials: true,                // allow cookies
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
   },
 });
 
@@ -96,7 +35,7 @@ const io = new Server(server, {
 // Share session with Socket.IO
 // ----------------------------
 io.use((socket, next) => {
-  sessionConfig(socket.request, {}, next); // attach session to socket.request
+  sessionConfig(socket.request, {}, next);
 });
 
 // Socket authentication
@@ -104,8 +43,6 @@ io.use(protectSocket);
 
 io.on("connection", (socket) => {
   console.log(`✅ User connected: ${socket.user.name} (${socket.user._id})`);
-
-  // Join personal notification room
   socket.join(`notifications:${socket.user._id}`);
   console.log(`User ${socket.user._id} joined notifications room`);
 
